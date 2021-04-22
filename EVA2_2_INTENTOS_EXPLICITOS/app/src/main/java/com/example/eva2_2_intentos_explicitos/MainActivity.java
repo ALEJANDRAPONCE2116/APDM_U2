b17d7852c7e0c31aebd1fbe38d6157549ed4b10b
Isela Alejandra Ponce de Leon <41318673+ALEJANDRAPONCE2116@users.noreply.github.com>
2021-04-22 12:55:51 -0600

    .
    .

--- EVA2_2_INTENTOS_EXPLICITOS/app/src/main/java/com/example/eva2_2_intentos_explicitos/MainActivity.java
+++ EVA2_2_INTENTOS_EXPLICITOS/app/src/main/java/com/example/eva2_2_intentos_explicitos/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.eva2_2_intentos_explicitos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                        //ACCION Y DATOS
+                        //ACTIVIDAD DE ORIGEN (contexto), CLASE DE LA ACTIVIDAD DESTINO (la que quiero ejecutar)
+                        //(obj que representa la actividad de origen), (clase de la actividad a lanzar)
+                        //(this), (OtraActividad)
+        //Declarar intento
+        intent = new Intent(this, OtraActividad.class);
+    }
+
+    public void Onclick(View v){
+        //LANZAR INTENTO IMPLICITO
+        //
+        startActivity(intent);
     }
 }
---
+++ EVA2_2_INTENTOS_EXPLICITOS/app/src/main/java/com/example/eva2_2_intentos_explicitos/OtraActividad.java
@@ -0,0 +1,15 @@
+package com.example.eva2_2_intentos_explicitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OtraActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra_actividad);
+    }
+
+}
